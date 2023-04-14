192bd8a5e75f1ced05e3b33a2f89f181fb3b84f7
semabraham99 <semaw190699@gmail.com>
2023-04-15 02:41:58 +0700

    button display

--- project_2/script.js
+++ project_2/script.js
@@ -1,46 +1,135 @@
 // validasi input no telepon 
 // validasi input nama dan no telepon 
 
-function getPos(){
-    let element = document.getElementById('kedua')
-    console.log(element.getBoundingClientRect())
+const soal = ["definisi satu adalah"," ____ ", "dimana kedua adalah"," ___ ","dan ketiga itu"]
+const opsi = ["jawaban 1","dua","jawaban 2","jawaban 3","lima","enam","Tujuh"]
+const kunci = [0,2,3]
+
+let last=0
+
+function test () {
+
+    buatSoal(soal)
+    buatButtonOpsi()
+    buatButtonIsian()
 }
 
-window.onload = function() {
-    // find the element that you want to drag.
-    var box = document.getElementById('coba1');
+function buttonTerpilih(jenis,nomor){
     
-    /* listen to the touchMove event,
-    every time it fires, grab the location
-    of touch and assign it to box */
-    
-    box.addEventListener('touchmove', function(e) {
-      // grab the location of touch
-      var touchLocation = e.targetTouches[0];
-      
-      // assign box new coordinates based on the touch.
-      box.style.left = touchLocation.pageX + 'px';
-      box.style.top = touchLocation.pageY + 'px';
-    })
-    
-    /* record the position of the touch
-    when released using touchend event.
-    This will be the drop position. */
-    
-    box.addEventListener('touchend', function(e) {
-      // current box position.
-      
-      var x = parseInt(box.style.left);
-      var y = parseInt(box.style.top);
-      
-      console.log("x",x)
-      box.style.left="10px"
-      box.style.top="10px"
+    if (jenis == "isian"){
+        
+        let buttonsIsian = document.getElementsByClassName('buttonIsian')
+        let buttonIsian = buttonsIsian[nomor]
+        let text = buttonIsian.innerText
 
-      box.style.transition=" all 0.4s" 
-    })
+        buttonIsian.innerText = ""
+        buttonIsian.style.display = "none"
+        
+        let buttonsOpsi = document.getElementsByClassName('buttonOpsi')
+        let index = opsi.indexOf(text)
+        let buttonOpsi = buttonsOpsi[index]
+
+        buttonOpsi.disabled = false
+       
+        last = nomor
+
+    }
+
+    if (jenis == "opsi"){
+        let buttonsOpsi = document.getElementsByClassName('buttonOpsi')
+        let buttonOpsi = buttonsOpsi[nomor]
+
+        buttonOpsi.disabled = "true"
+
+        let buttonsIsian = document.getElementsByClassName('buttonIsian')
+        let buttonIsian = buttonsIsian[last]
+        
+        buttonIsian.innerText = opsi[nomor]
+        buttonIsian.style.display = "block"
+        
+        last += 1
+    }
+}
+
+function test2(){
+    let hasil = opsi.indexOf("dua")
+    console.log ("hasil")
     
-  }
+    console.log (hasil)
+}
+
+function buatButtonOpsi(){
+    let container = document.getElementById("divOpsi")
+    let jumlah = opsi.length
+
+    for ( let i = 0 ; i < jumlah ; i++){
+        
+        let button = document.createElement('button')
+        
+        button.className = "buttonOpsi"
+        button.innerText = opsi[i]
+        
+        button.onclick = function(){buttonTerpilih("opsi",i)}
+    
+        container.appendChild(button)
+    }
+}
+
+function updateButton(index, isi){
+    let buttons = document.getElementsByClassName('buttonIsian')
+    let button = buttons[index]
+    button.textContent = isi
+    button.style.display="block"
+}
+
+function buatButtonIsian(){
+    let isian = document.getElementsByClassName('isian')
+    let jumlah = isian.length
+    let isi = ""    
+
+    for (let i = 0;i < jumlah; i++){
+        
+        let rect = isian[i].getBoundingClientRect()
+        
+        let posX = rect.x
+        let posY = rect.y
+        let lebar = rect.width
+        let tinggi = rect.height
+
+        let button = document.createElement('button')
+
+        button.className = "buttonIsian"
+        button.innerText = isi
+
+        button.style.top = (posY + 10) + "px"
+        button.style.left = (posX - 10) + "px"
+        button.style.width = (lebar - 10)  + "px"
+        button.style.height = (tinggi/2) + "px"
+
+        button.style.display = "none"
+        button.onclick = function(){buttonTerpilih("isian",i)}
+
+        document.body.appendChild(button)
+    }
+
+}
+
+function buatSoal (){
+    
+    let container = document.getElementById("divSoal")
+    let content = ""
+    
+    for (let i = 0 ; i < soal.length; i++){
+        if (i%2 == 0){
+            content+= `<p class="teks"> ${soal[i]} </p>`
+        }
+        else{
+            content+= `<p class="isian"> ${soal[i]} </p>`
+        }
+    }
+    container.innerHTML = content
+}
+
 
 function buatRequest(nama,telepon){
 
