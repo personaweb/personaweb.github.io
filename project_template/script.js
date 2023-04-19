fa0dd0b0c8443d29edf09a389c7d8d6b5efce17b
semabraham99 <semaw190699@gmail.com>
2023-04-19 18:45:28 +0700

    test json

---
+++ project_template/script.js
@@ -0,0 +1,35 @@
+// goal  
+
+
+function buatRequest(nama,telepon){
+
+    console.log("pesanan diproses")
+    
+    // fetch pesanan 
+    fetch("https://script.google.com/macros/s/AKfycbx-RSvI6WBRyHCTUTReqLP8xmQxigH7Jk4hq1AtcGai69EP2uCvlrjLo5JI71E1nLVm/exec?nama="+ String(nama) + "&noTelp=" + String(telepon)
+        
+    ).then(response => {
+        console.log(response);
+        
+    }).catch(err => {
+        console.log("Error:" + err);
+        
+    });
+}
+
+
+async function fetchLogin(nama,telepon){
+    
+    const response = await fetch("https://script.google.com/macros/s/AKfycbx-RSvI6WBRyHCTUTReqLP8xmQxigH7Jk4hq1AtcGai69EP2uCvlrjLo5JI71E1nLVm/exec?nama="+ String(nama) + "&noTelp=" + String(telepon),{method: 'GET'});
+    const data = await response.json();
+    let hasil = JSON.parse(data)
+    if (hasil == -1){
+        pesan("id tidak ditemukan")
+    }
+    else{
+        pesan("id adalah : " + hasil)
+    }
+    
+  
+    return hasil
+}
